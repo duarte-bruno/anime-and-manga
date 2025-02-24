b97c2e5298cf6175d58384a8824f812a302fc23c
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2025-02-24 20:46:23 -0300

    Project created.

---
+++ AnimeAndManga/AnimeAndMangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAndMangaApp.swift
+//  AnimeAndManga
+//
+//  Created by Bruno Duarte on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAndMangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
